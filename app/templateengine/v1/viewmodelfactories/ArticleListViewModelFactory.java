5a1a88dc09078c8752b6bff8b9e45a5cb898230a
Kevin Riesen <kevin.riesen@informaticon.com>
2023-05-23 13:28:50 +0200

    fix: parse int fix

--- app/templateengine/v1/viewmodelfactories/ArticleListViewModelFactory.java
+++ app/templateengine/v1/viewmodelfactories/ArticleListViewModelFactory.java
@@ -5,6 +5,7 @@ import play.mvc.Http;
 import templateengine.v1.viewmodels.ArticleListViewModel;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 public class ArticleListViewModelFactory implements IViewModelFactory<ArticleListViewModel>{
 
@@ -16,9 +17,18 @@ public class ArticleListViewModelFactory implements IViewModelFactory<ArticleLis
     @Override
     public ArticleListViewModel buildViewModel(Http.Request request) {
         int maxRows = request.queryString("maxRows")
-                .map(Integer::valueOf)
+                .flatMap(this::parseInt)
                 .orElse(10);
 
         return new ArticleListViewModel(Article.findAll(maxRows));
+    }
+
+    private Optional<Integer> parseInt(String value){
+        try {
+            return Optional.of(Integer.valueOf(value));
+        }
+        catch (NumberFormatException e) {
+            return Optional.empty();
+        }
     }
 }
